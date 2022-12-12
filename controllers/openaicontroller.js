800aa9854d9c6a06592956749d1f79f9900839a8
kenrick campbell <kennyg8810@gmail.com>
2022-12-12 18:54:11 -0500

    added ability to sent api request

--- controllers/openaicontroller.js
+++ controllers/openaicontroller.js
@@ -11,11 +11,27 @@ const generateImage = async (req, res) => {
             prompt: 'triangle power',
             n: 1,
             size: '512x512'
+        });
+
+        const imageUrl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl
         })
     } catch (error) {
-
-    }
-    
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+          
+        res.status(400).json({
+            success: false,
+            error: 'the image could not be generated'
+        });
+    }    
 };
 
 module.exports = { generateImage }
